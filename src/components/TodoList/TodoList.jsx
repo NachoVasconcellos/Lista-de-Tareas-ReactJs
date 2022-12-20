2ce5326e036c54988c7cc06f7ad62b1ab22cdaba
NachoVasconcellos <nachovasconcellos@gmail.com>
2022-12-20 16:10:38 -0300

    Lista de Tareas

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,29 @@
+import { Todo } from "../Todo/Todo"
+import { TodoFilters } from "../TodoFilters"
+
+const TodoList = ({ todos, handleSetComplete, handleClearComplete, handleDelete, activeFilter, showAllTodos, showActiveTodos, showCompletedTodos }) => {
+    return (
+        <div className="flex flex-col mt-7 rounded overflow-hidden shadow-2xl">
+            {todos.map(todo => {
+                return(
+                    <Todo 
+                    key={todo.id} 
+                    todo={todo} 
+                    handleSetComplete={handleSetComplete}
+                    handleDelete={handleDelete}
+                    />
+                )
+            })}
+            <TodoFilters
+                activeFilter={activeFilter}
+                total={todos.lenght}
+                showAllTodos={showAllTodos}
+                showActiveTodos={showActiveTodos}
+                showCompletedTodos={showCompletedTodos}
+                handleClearComplete={handleClearComplete}
+            />
+        </div>
+    )
+}
+
+export { TodoList }
